e800d1e16a47ccb977c610e1d76435a611096f33
Riccardo Stocker <riccardo.stocker@scinteco.com>
2026-04-24 13:31:45 +0200

    Courses field card renderer

--- js/app.js
+++ js/app.js
@@ -26,3 +26,19 @@ function showTab(){
 showTab();
 document.getElementById('home-panel').classList.add('active');
 document.querySelector('#nav [data-target="home-panel"]').classList.add('active');
+
+function loadCourses() {
+    fetch('content/fields.json')
+        .then(response => response.json())
+        .then(data => {
+            const dataPanel = document.getElementById('courses-panel');
+
+            data.fields.forEach(field => {
+                const card = document.createElement('div');
+                card.textContent = field.name;
+                card.style.backgroundColor = field.color;
+                dataPanel.appendChild(card);
+            });
+        });
+}
+loadCourses();
